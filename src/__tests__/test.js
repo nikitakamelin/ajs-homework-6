6fc366fc1ff3c48db3ba6dc3f6db1cbea1e0d82f
NK Test <nkamelin@gmail.com>
2023-07-29 09:29:32 +0300

    tests done

---
+++ src/__tests__/test.js
@@ -0,0 +1,23 @@
+import Validator from "../js/Validator";
+
+const dataList = [
+	['Ni54kik', new Validator('Ni54kik'), true],
+	['Nik', new Validator('Nik'), true],
+	['Nk', new Validator('Nk'), true],
+	['N888k', new Validator('N888k'), true],
+	['Ni66-kik', new Validator('Ni66-kik'), true],
+	['Ni66_ki', new Validator('Ni66_ki'), true],
+	['N8K', new Validator('N8K'), true],
+	['_Nik', new Validator('_Nik'), false],
+	['9Nikdfd', new Validator('9Nikdfd'), false],
+	['Nikdfd9', new Validator('Nikdfd9'), false],
+	['-Nik', new Validator('-Nik'), false],
+	['Nik-', new Validator('Nik-'), false],
+	['Ni6666kik', new Validator('Ni6666kik'), false],
+];
+
+test.each(dataList)('testing Validator with name %s ', (_, object, expected) => {
+  const result = object.validateUsername();
+
+  expect(result).toBe(expected);
+});
---
+++ src/js/Validator.js
@@ -0,0 +1,8 @@
+export default class Validator {
+	constructor(name) {
+	  this.name = name;
+	}
+	validateUsername() {
+		 return /^[^-_0-9]+[\w-]{0,3}[^-_0-9]+$/gi.test(this.name);
+	}
+ }
