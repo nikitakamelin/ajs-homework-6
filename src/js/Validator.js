ef4ef5b944ad4e6293b3ff6e8c41923771d2935c
NK Test <nkamelin@gmail.com>
2023-07-29 09:32:23 +0300

    eslint check done

--- src/js/Validator.js
+++ src/js/Validator.js
@@ -1,8 +1,9 @@
 export default class Validator {
-	constructor(name) {
-	  this.name = name;
-	}
-	validateUsername() {
-		 return /^[^-_0-9]+[\w-]{0,3}[^-_0-9]+$/gi.test(this.name);
-	}
- }
+  constructor(name) {
+    this.name = name;
+  }
+
+  validateUsername() {
+    return /^[^-_0-9]+[\w-]{0,3}[^-_0-9]+$/gi.test(this.name);
+  }
+}
